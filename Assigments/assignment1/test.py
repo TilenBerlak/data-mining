f020ff6f2b4002be1f5d50cbb1c626d4f0918831
Tilen Berlak <44645168+TilenBerlak@users.noreply.github.com>
2022-11-04 17:33:12 +0100

    Delete Assigments directory

--- Assigments/assignment1/test.py
+++
@@ -1,8 +0,0 @@
-from csv import DictReader
-
-reader = DictReader(open('movies.csv', 'rt', encoding='utf-8'))
-for row in reader:
-    user = row["userId"]
-    movie = row["movieId"]
-    rating = row["rating"]
-    timestamp = row["timestamp"]
